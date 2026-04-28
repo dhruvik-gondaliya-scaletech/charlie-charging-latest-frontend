80db2fbd824db92311e9012f2526ae2c72b172be
dhruvik-gondaliya-scaletech <dhruvik.gondaliya@scaletech.xyz>
2026-04-28 14:14:06 +0530

    Fix: domain name for url creation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://example.com',
+    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://example.com',
     generateRobotsTxt: true,
     changefreq: 'daily',
     priority: 0.7,
